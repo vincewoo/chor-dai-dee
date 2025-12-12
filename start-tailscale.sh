8ba45dd8fb33e15cf7bf6e468558b5477d12c424
vincewoo <vincewoo@gmail.com>
2025-12-12 13:37:23 -0800

    Fixing some tailscale issues

--- start-tailscale.sh
+++ start-tailscale.sh
@@ -11,9 +11,13 @@ done
 # Wait a few more seconds for the daemon to be fully ready
 sleep 5
 
-# Run the serve command
+# Run the serve command first
 echo "Configuring Serve..."
 tailscale --socket=/tmp/tailscaled.sock serve https / http://localhost:3000
+
+# Enable funnel to expose to public internet
+echo "Enabling Funnel..."
+tailscale --socket=/tmp/tailscaled.sock funnel 443 on
 
 # Keep the container running (tail the logs of the background process)
 wait
